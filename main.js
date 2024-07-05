e7b4772f6ebff8715c4060bf8bef805afbf2cdeb
shrutigupta2003 <138881084+shrutigupta2003@users.noreply.github.com>
2024-07-05 16:57:52 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const populate = async (value, currency) => {
+    let myStr = ""
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_ksILtnaCk8qkbpEIyhcble1utUE2srHPGRvgOZ1A&base_currency=" + currency;
+
+    let response = await fetch(url)
+    let rJson = await response.json()
+    document.querySelector(".output").style.display="block";
+
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += `
+    <tr>
+        <td>${key}</td>
+        <td>${rJson["data"][key]["code"]}</td>
+        <td>${Math.round(rJson["data"][key]["value"]*value)}</td>
+    </tr>
+    `
+    }
+    const tablebody = document.querySelector("tbody");
+    tablebody.innerHTML = myStr;
+}
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const currency = document.querySelector("select[name='currency']").value;
+    populate(value, currency);
+})
+
